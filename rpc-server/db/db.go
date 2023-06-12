095fe392570f445c86b28bc2c51e9bd900091f2a
argon <argon@nogra.xyz>
2023-06-12 17:19:27 +0800

    Create new DB class for better structure

---
+++ rpc-server/db/db.go
@@ -0,0 +1,55 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type DB struct {
+	conn *sql.DB
+}
+
+// CreateDB Returns an instance of a DB connection to be used
+func CreateDB(dbHost, dbName, dbUsername, dbPassword string) (*DB, error) {
+	database_url := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUsername, dbPassword, dbHost, dbName)
+	conn, err := sql.Open("mysql", database_url)
+	if err != nil {
+		return nil, err
+	}
+	return &DB{conn: conn}, nil
+}
+
+// Close closes database connection
+func (db *DB) Close() error {
+	return db.conn.Close()
+}
+
+// ExecInsert used for insert query, returns ID of inserted row
+func (db *DB) ExecInsert(q string, args ...interface{}) (int64, error) {
+	result, err := db.conn.Exec(q, args...)
+	if err != nil {
+		// Error encountered
+		return 0, err
+	}
+	return result.LastInsertId()
+}
+
+// ExecSelectOne used for SELECT query for a single row, returns results
+func (db *DB) ExecSelectOne(q string, args ...interface{}) (*sql.Row, error) {
+	return db.conn.QueryRow(q, args...), nil
+}
+
+// ExecSelectMany used for SELECT query of multiple rows, returns results
+func (db *DB) ExecSelectMany(q string, args ...interface{}) (*sql.Rows, error) {
+	return db.conn.Query(q, args...)
+}
+
+// ExecUpdate used for UPDATE query, returns number of updated rows
+func (db *DB) ExecUpdate(q string, args ...interface{}) (int64, error) {
+	result, err := db.conn.Exec(q, args...)
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
